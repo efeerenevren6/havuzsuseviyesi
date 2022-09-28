c0131996ff96eb3ddf1e564da997de04d36b565a
efeerenevren6 <efeerenerven006@gmail.com>
2022-09-28 11:48:04 +0300

    Initial commit

---
+++ src/havuzsuseviyesi/Main.java
@@ -0,0 +1,26 @@
+package havuzsuseviyesi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//seviye > 0 && seviye <10   ||
+		double suseviyesi=25;
+		if(suseviyesi<10.1) {
+			System.out.println("||");
+		}
+		//seviye > 10 && seviye <20  ||||
+		if(suseviyesi>=20&&suseviyesi<30.1) {
+			System.out.println("||||");
+		}	
+		//seviye > 20 && seviye <30  ||||||
+		//seviye > 30 && seviye <40  ||||||||
+		//seviye > 40 && seviye <50  ||||||||||
+		//seviye > 50 && seviye <60  ||||||||||||
+		//seviye > 60 && seviye <70  ||||||||||||||
+		//seviye > 70 && seviye <80  ||||||||||||||||
+		//seviye > 80 && seviye <90  ||||||||||||||||||
+		//seviye > 90 && seviye <100 ||||||||||||||||||||
+
+	}
+
+}
