f1cfed1c1cb1fd46fededea12e8148c4997ac106
efeerenevren6 <efeerenerven006@gmail.com>
2022-09-28 11:48:29 +0300

    qqweqw

--- src/havuzsuseviyesi/Main.java
+++ src/havuzsuseviyesi/Main.java
@@ -9,18 +9,46 @@ public class Main {
 			System.out.println("||");
 		}
 		//seviye > 10 && seviye <20  ||||
-		if(suseviyesi>=20&&suseviyesi<30.1) {
+		if(suseviyesi>=10&&suseviyesi<20.1) {
 			System.out.println("||||");
 		}	
 		//seviye > 20 && seviye <30  ||||||
+		if(suseviyesi>=20.1&&suseviyesi<30.1) {
+			System.out.println("||||||");
+		}
 		//seviye > 30 && seviye <40  ||||||||
+		if(suseviyesi>=30.1&&suseviyesi<40.1)  {
+			System.out.println("||||||||");
+		}
 		//seviye > 40 && seviye <50  ||||||||||
+		if(suseviyesi>=40.1&&suseviyesi<50.1) {
+			System.out.println("||||||||||");
+		}
 		//seviye > 50 && seviye <60  ||||||||||||
+		if(suseviyesi>=50.1&&suseviyesi<60.1) {
+			System.out.println("||||||||||||");
+		}
 		//seviye > 60 && seviye <70  ||||||||||||||
+		if(suseviyesi>=60.1&&suseviyesi<70.1) {
+			System.out.println("||||||||||||||");
+		}
 		//seviye > 70 && seviye <80  ||||||||||||||||
+		if(suseviyesi>=70.1&&suseviyesi<80.1) {
+			System.out.println("||||||||||||||||");
+		}
 		//seviye > 80 && seviye <90  ||||||||||||||||||
+		if(suseviyesi>=80.1&&suseviyesi<90.1) {
+			System.out.println("||||||||||||||||||");
+		}
 		//seviye > 90 && seviye <100 ||||||||||||||||||||
+		if(suseviyesi>=90.1&&suseviyesi<100.1) {
+			System.out.println("||||||||||||||||||||");
+		}
 
+		
+		
+		
+		
 	}
 
 }
